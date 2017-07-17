0945975e333e7ac48e4385273b974b2f345cfdeb
Jingjing <shijingjing1221@gmail.com>
2017-07-17 20:07:02 +0800

    Add sidebar active class

--- 04_generate_muluhtml_file.sh
+++ 04_generate_muluhtml_file.sh
@@ -5,6 +5,7 @@ github-markdown SUMMARY.md > $muluFile
 sed -i 's/\.md//g' $muluFile
 sed -i 's/href="/href="\//g' $muluFile
 sed -i '1,3d' $muluFile
+sed -i 's/href="\(.*\)"/ \{% if page.url == "\1.html" %\} class="active"\{% endif %\} href="\1"/' _includes/mulu.html
 
 muluFile=mulu.md
 sed -i 's/\.md//g' $muluFile
